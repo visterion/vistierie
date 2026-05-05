72c2a850877ff9f13b1b2cc79039bc33fc4a41f3
visterion <46775082+visterion@users.noreply.github.com>
2026-05-05 22:13:16 +0200

    chore(slice1): scaffold Spring Boot 4 / JDK 25 module

---
+++ java-server/src/main/java/de/vesterion/vistierie/VistierieApplication.java
@@ -0,0 +1,11 @@
+package de.vesterion.vistierie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VistierieApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(VistierieApplication.class, args);
+    }
+}
